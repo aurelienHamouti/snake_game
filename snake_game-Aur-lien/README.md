302c7d8ebcc1e7bf764512b5689ffa047de470a7
manuu11 <81250806+manuu11@users.noreply.github.com>
2021-05-19 20:44:55 +0200

    Delete snake_game-Aur-lien directory

--- snake_game-Aur-lien/README.md
+++
@@ -1,2 +0,0 @@
-# snake_game
-Little snake game as part of a python programming course at the university of Lausanne
--- snake_game-Aur-lien/snake_game.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-import src.welcome as welcomePage
-try:
-    welcomePage.start() #Start welcome page
-except ValueError:
-    print("An exception occurred : " + ValueError)
