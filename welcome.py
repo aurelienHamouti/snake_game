1e245c5f440ee9c35c298cecc29be4d21a40efd0
manuu11 <81250806+manuu11@users.noreply.github.com>
2021-05-21 21:50:47 +0200

    Add files via upload

--- welcome.py
+++ welcome.py
@@ -2,6 +2,7 @@
 from tkinter import *
 import tkinter.font as TkFont
 from src import snake as snake
+import os
 
 class Welcome(Frame):
 
@@ -15,8 +16,7 @@ class Welcome(Frame):
         #self._drawing = Canvas(width=800,height=500)
         self._me.config(background=backgroundColor)
 
-        self._welcome = Label(text = "Welcome on the DCS snake game ssssss !")
-        self._levelChoice = Label(text = "Choose your level :")
+        
         self._slugLevel = Button(text = "Slug")
         self._CoralLevel = Button(text = "Coral")
         self._pythonLevel = Button(text = "Python")
@@ -39,44 +39,29 @@ class Welcome(Frame):
         levelLabelFont = TkFont.Font(family="Great Vibes", size=15, weight="bold")
         levelFont = TkFont.Font(family="Showcard Gothic", size=20, weight="bold")
 
-        #welcome message label
-        self._welcome['font'] = titleFont
-        self._welcome['bg'] = "#00cc00"
-        self._levelChoice['fg'] = "white"
-        #self._welcome.grid(row=0, column=0, columnspan=1)
-        self._welcome.pack()
-
-        #level message label
-        self._levelChoice['font'] = levelLabelFont
-        self._levelChoice['bg'] = "#00cc00"
-        self._levelChoice['fg'] = "white"
-        self._levelChoice.pack()
-        self._levelChoice.place(relx=0.5, rely=0.20, anchor=CENTER)
+        
 
         #slug level button
         self._slugLevel['font'] = levelFont
         self._slugLevel['activebackground'] = "#ffff00"
         #self._slugLevel.grid(row=2, sticky=N, padx=20, pady=20)
-        self._slugLevel.pack()
-        self._slugLevel.place(relx=0.325, rely=0.40, anchor=CENTER)
+        self._slugLevel.place(relx=0.35, rely=0.52, anchor=CENTER)
+        
        
         #coral level button
         self._CoralLevel['font'] = levelFont
         self._CoralLevel['activebackground'] = "#ffff00"
-        self._CoralLevel.pack()
-        self._CoralLevel.place(relx=0.5, rely=0.40, anchor=CENTER)
+        self._CoralLevel.place(relx=0.5, rely=0.52, anchor=CENTER)
 
         #python level button
         self._pythonLevel['font'] = levelFont
         self._pythonLevel['activebackground'] = "#ffff00"
-        self._pythonLevel.pack()
-        self._pythonLevel.place(relx=0.7, rely=0.40, anchor=CENTER)
+        self._pythonLevel.place(relx=0.65, rely=0.52, anchor=CENTER)
 
         #exit button
         self._exit['font'] = "Helvetica 20 bold"
         self._exit['activebackground'] = "#ff0000"
-        self._exit.pack()
-        self._exit.place(relx=0.5, rely=0.90, anchor=CENTER)
+        self._exit.place(relx=0.95, rely=0.95, anchor=CENTER)
 
         #menu
         menu = Menu(self._menubar, tearoff=0)
@@ -86,7 +71,7 @@ class Welcome(Frame):
         
         menu.add_cascade(label="Help", menu=helpmenu)
         helpmenu.add_command(label="user manual", command=self.showHelp)
-        helpmenu.add_command(label="license")
+        helpmenu.add_command(label="license", command=self.showLicence)
         menu.add_separator()
         menu.add_command(label="Quitter", command=self.exit)
         
@@ -127,15 +112,27 @@ class Welcome(Frame):
         pass
 
     def showHelp(self):
-        usermanual=Tk()
+        usermanual=Toplevel()
         usermanual.title("User Manual")
-        usermanual.geometry("707x1000+400+200")
-        canvas = Canvas(usermanual, width = 707, height = 1000)      
+        usermanual.geometry("636x900+400+200")
+        canvas = Canvas(usermanual, width = 636, height = 900)      
         canvas.pack()      
-        img = PhotoImage(file="manuel.png")      
+        mypath=os.path.join("ressources", "images", "manuel.png")
+        img=PhotoImage(file=mypath)      
         canvas.create_image(0,0, anchor=NW, image=img)      
         usermanual.resizable(width=False, height=False)
         usermanual.mainloop()
+
+    def showLicence(self):
+        license=Tk()
+        license.title("License")
+        license.geometry("345x400+500+200")
+        label1=Label(license, text="Snake DCS", font=("Courier New", 25, "bold", "underline"))
+        label1.place(x=100, y=15)
+        label2=Label(license, text="Copyright (C) <2021> <Aurélien, Marc, Manuel> \n This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or(at your option) any later version.\n \n This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n \n You should have received a copy of the GNU General Public License along with this program.If not, see <http://www.gnu.org/licenses/>.", wraplength=300, justify="left", font=("Courier New", 12))
+        label2.place(x=15, y=75)
+        license.resizable(width=False, height=False)
+        license.mainloop()
 
     def exit(self):
         self._me.destroy()
@@ -144,5 +141,10 @@ def start():
     root = Tk()
     root.iconbitmap(r'ressources\images\blackSnakeIcon.ico')
     #iconphoto(True, PhotoImage(file="./ressources/blackSnakeIcon.ico"))
-    program = Welcome(root, "Snake DCS", "800x500+250+50", "#00cc00")
+    bgpath=os.path.join("ressources", "images", "pageA.png")
+    bg=PhotoImage(file=bgpath)
+    bglabel=Label(root, image=bg)
+    bglabel.place(x=0, y=0, relwidth=1, relheight=1)
+    program = Welcome(root, "Snake DCS", "900x594+250+50")
+    root.resizable(width=False, height=False)
     program.mainloop()
